9a37f3dfd13d27bb91c9e02d3349e8455f66b143
bezawit <bezi9182@gmail.com>
2022-04-09 16:30:40 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
 /**
  *printing using puts syntax
 *main: entry point
+*return always 0 (success)
 */
-* int main(void)
+ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"\n);
 	return (0);
