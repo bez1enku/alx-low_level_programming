6808ec9874b4ee7c728cbcf1bf16a77bf07c24eb
bezawit <bezi9182@gmail.com>
2022-04-09 16:35:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
 */
  int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 	/*return : exit point and  0 imply (sucess)*/
 }
