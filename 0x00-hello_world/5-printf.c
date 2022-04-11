86406c0389410e9a6cf49ef0cada6cecbe33cd82
bezawit <bezi9182@gmail.com>
2022-04-11 11:59:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>>
+/**
+ *main -Entry point
+*hello using printf
+*Return: return 0 in successful print
+*/
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+       return (0);
+}       
